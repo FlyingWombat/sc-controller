2c69b43ce34e50ea9a3652d1206015e8582ba68a
FlyingWombat <25214020+FlyingWombat@users.noreply.github.com>
2018-08-02 21:15:16 -0400

    added conditional in scc.lib.__init__.py to import from System enum.py if PyVer >= 3.0
    the enum.py that was previously included in scc.lib was renamed to enum2.py
    it is a fork of the 3.4 enum.py to make it python2 compatible
    keeping it around for python2 support (maybe shouldn't bother)

--- scc/lib/__init__.py
+++ scc/lib/__init__.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 from __future__ import absolute_import
-from .enum import Enum, IntEnum, unique
+import sys
+
+#This will import System python enum.py if PyVer > 3.0
+#Otherwise will import Python 2 compatible enum.py,
+#modified from https://github.com/certik/enum34
+
+pyver = float('%s.%s' % sys.version_info[:2])
+
+if pyver >= 3.0:
+    from enum import Enum, IntEnum, unique
+else:
+    from .enum2 import Enum, IntEnum, unique
+
